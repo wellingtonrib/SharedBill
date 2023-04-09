f8968179216f6ef28ea472d01560e7873c2069c5
Wellington Ribeiro <wellington.rib@gmail.com>
2023-04-08 21:29:24 -0300

    Dependency analysis plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.gms) apply false
     alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.dependency.analysis) apply true
 }
 
 tasks.register("clean", Delete::class) {
