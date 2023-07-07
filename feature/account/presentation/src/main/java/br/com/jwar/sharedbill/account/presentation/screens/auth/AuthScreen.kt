d1e770fe95115e13acd7d06de3311643a5153143
Wellington Ribeiro <wellington.rib@gmail.com>
2023-07-07 17:50:46 -0300

    Login screen update

--- feature/account/presentation/src/main/java/br/com/jwar/sharedbill/account/presentation/screens/auth/AuthScreen.kt
+++ feature/account/presentation/src/main/java/br/com/jwar/sharedbill/account/presentation/screens/auth/AuthScreen.kt
@@ -1,18 +1,32 @@
 package br.com.jwar.sharedbill.account.presentation.screens.auth
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.com.jwar.sharedbill.account.presentation.R
 import br.com.jwar.sharedbill.account.presentation.screens.auth.AuthContract.State
 import br.com.jwar.sharedbill.account.presentation.screens.auth.AuthContract.State.*
 import br.com.jwar.sharedbill.account.presentation.screens.auth.components.SignInButton
 import br.com.jwar.sharedbill.core.designsystem.components.LoadingContent
+import br.com.jwar.sharedbill.core.designsystem.theme.AppTheme
 import br.com.jwar.sharedbill.core.designsystem.theme.SharedBillTheme
+import br.com.jwar.sharedbill.core.designsystem.theme.VerticalSpacerLarge
+import br.com.jwar.sharedbill.core.designsystem.R as DS
 
 @Composable
 fun AuthScreen(
@@ -23,10 +37,29 @@ fun AuthScreen(
         is Loading -> LoadingContent()
         is Idle -> {
             Box(
-                modifier = Modifier.fillMaxSize(),
+                modifier = Modifier
+                    .background(AppTheme.colors.primaryContainer)
+                    .fillMaxSize(),
                 contentAlignment = Alignment.Center
             ) {
-                SignInButton(onSignInClick)
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        modifier = Modifier.size(200.dp),
+                        painter = painterResource(DS.drawable.app_icon),
+                        contentDescription = stringResource(R.string.description_app_logo)
+                    )
+                    Text(
+                        text = stringResource(DS.string.app_name),
+                        fontSize = 32.sp,
+                        fontWeight = FontWeight(600),
+                        color = AppTheme.colors.onPrimaryContainer,
+                        style = AppTheme.typo.titleLarge
+                    )
+                    VerticalSpacerLarge()
+                    SignInButton(onSignInClick)
+                }
             }
         }
     }
