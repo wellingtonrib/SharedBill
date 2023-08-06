5c06707d10895f3e178e548e7f82c80682252c90
Wellington Ribeiro <wellington.rib@gmail.com>
2023-08-06 20:16:17 -0300

    UserAvatarStack NotShowingIndicator

--- feature/groups/presentation/src/main/java/br/com/jwar/groups/presentation/ui/group_list/components/GroupListItem.kt
+++ feature/groups/presentation/src/main/java/br/com/jwar/groups/presentation/ui/group_list/components/GroupListItem.kt
@@ -1,20 +1,13 @@
 package br.com.jwar.groups.presentation.ui.group_list.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.material.DismissDirection
 import androidx.compose.material.SwipeToDismiss
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,9 +20,7 @@ import br.com.jwar.sharedbill.core.designsystem.components.UserAvatarStack
 import br.com.jwar.sharedbill.core.designsystem.theme.AppTheme
 import br.com.jwar.sharedbill.core.designsystem.theme.SharedBillTheme
 import br.com.jwar.sharedbill.core.designsystem.theme.VerticalSpacerSmall
-import br.com.jwar.sharedbill.core.designsystem.theme.fillMaxWidthPaddingMedium
 import br.com.jwar.sharedbill.core.designsystem.theme.paddingMedium
-import br.com.jwar.sharedbill.core.designsystem.theme.paddingSmall
 import br.com.jwar.sharedbill.groups.presentation.R
 
 @Composable
@@ -73,7 +64,8 @@ fun GroupListItem(
                 UserAvatarStack(
                     users = group.members.map { it.toUserUiModel() },
                     avatarSize = 32.dp,
-                    overlap = 10.dp
+                    overlap = 8.dp,
+                    maxAvatars = 5
                 )
             }
         }
