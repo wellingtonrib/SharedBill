c01b9715491e9f0c8281939d955ba7b14b5780e0
Wellington Ribeiro <wellington.rib@gmail.com>
2023-09-09 20:51:09 -0300

    Firebase app check setup

--- app/src/main/java/br/com/jwar/sharedbill/App.kt
+++ app/src/main/java/br/com/jwar/sharedbill/App.kt
@@ -1,7 +1,27 @@
 package br.com.jwar.sharedbill
 
 import android.app.Application
+import com.google.firebase.appcheck.AppCheckProviderFactory
+import com.google.firebase.appcheck.ktx.appCheck
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.initialize
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class App: Application()
+class App: Application() {
+
+    @Inject
+    lateinit var appCheckProviderFactory: AppCheckProviderFactory
+
+    override fun onCreate() {
+        super.onCreate()
+
+        initializeFirebase()
+    }
+
+    private fun initializeFirebase() {
+        Firebase.initialize(context = this)
+        Firebase.appCheck.installAppCheckProviderFactory(appCheckProviderFactory)
+    }
+}
