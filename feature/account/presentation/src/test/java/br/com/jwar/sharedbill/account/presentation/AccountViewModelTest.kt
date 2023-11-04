0445aa4d98d90d336b831acc7f862e8bc630e359
Wellington Ribeiro <wellington.rib@gmail.com>
2023-11-04 18:00:28 -0300

    Unit tests for AccountViewModel and GroupDetailsViewModel

--- feature/account/presentation/src/test/java/br/com/jwar/sharedbill/account/presentation/AccountViewModelTest.kt
+++ feature/account/presentation/src/test/java/br/com/jwar/sharedbill/account/presentation/AccountViewModelTest.kt
@@ -7,15 +7,19 @@ import br.com.jwar.sharedbill.account.domain.usecases.*
 import br.com.jwar.sharedbill.account.presentation.mappers.UserToUserUiModelMapper
 import br.com.jwar.sharedbill.account.presentation.ui.account.AccountContract
 import br.com.jwar.sharedbill.account.presentation.ui.account.AccountViewModel
+import br.com.jwar.sharedbill.core.designsystem.model.UserUiModel
 import br.com.jwar.sharedbill.testing.CoroutinesTestRule
-import br.com.jwar.sharedbill.testing.Fakes
 import io.mockk.*
 import kotlin.test.assertEquals
-import kotlin.test.assertNotNull
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
 import org.junit.Rule
 import org.junit.Test
+import kotlin.test.assertTrue
 
 @OptIn(ExperimentalCoroutinesApi::class)
 internal class AccountViewModelTest {
@@ -35,58 +39,78 @@ internal class AccountViewModelTest {
     }
 
     @Test
-    fun `GIVEN viewModel WHEN GetUser event SHOULD call getUserCase`() = runTest {
-        //GIVEN
-        coEvery { getCurrentUserUseCase() } returns Result.success(User())
-        //WHEN
+    fun `GIVEN a logged in user WHEN init SHOULD get current and map the user`() = runTest {
+        val user = User()
+        prepareScenario(userResult = Result.success(user))
+
         viewModel.emitEvent { AccountContract.Event.OnInit }
-        //THEN
+
         coVerify { getCurrentUserUseCase() }
+        verify { userToUserUiModelMapper.mapFrom(user) }
     }
 
     @Test
-    fun `GIVEN viewModel WHEN GetUser event SHOULD update the ui state as Loading`() = runTest {
-        //GIVEN
-        coEvery { getCurrentUserUseCase() } returns Result.success(User())
-        //WHEN
+    fun `GIVEN a logged in user WHEN init SHOULD update the ui state`() = runTest {
+        val userUiModel = UserUiModel()
+        val stateList = mutableListOf<AccountContract.State>()
+        prepareScenario(
+            stateList = stateList,
+            userUiModelResult = userUiModel
+        )
+
         viewModel.emitEvent { AccountContract.Event.OnInit }
-        //THEN
-        val state = viewModel.uiState.value as? AccountContract.State.Loading
-        assertNotNull(state)
+
+        assertTrue(stateList.first().isLoading)
+        assertEquals(stateList.drop(1).first().uiModel, userUiModel)
     }
 
     @Test
-    fun `GIVEN there is an User WHEN GetUser event SHOULD update the ui state as Loaded with User`() = runTest {
-        //GIVEN
-        val user = Fakes.makeUser()
-        coEvery { getCurrentUserUseCase() } returns Result.success(user)
-        //WHEN
+    fun `GIVEN UserNotFoundException WHEN init SHOULD send NavigateToAuth effect`() = runTest {
+        val effectList = mutableListOf<AccountContract.Effect>()
+        prepareScenario(
+            effectList = effectList,
+            userResult = Result.failure(UserException.UserNotFoundException)
+        )
+
         viewModel.emitEvent { AccountContract.Event.OnInit }
-        //THEN
-        val state = viewModel.uiState.value as? AccountContract.State.Loaded
-        assertNotNull(state)
+
+        assertEquals(AccountContract.Effect.NavigateToAuth, effectList.first())
     }
 
     @Test
-    fun `GIVEN there is an exception WHEN GetUser event SHOULD update the ui state as Error with message`() = runTest {
-        //GIVEN
-        val exception = Exception("Generic Exception")
-        coEvery { getCurrentUserUseCase() } returns Result.failure(UserException.UserNotFoundException)
-        //WHEN
-        viewModel.emitEvent { AccountContract.Event.OnInit }
-        //THEN
-        val state = viewModel.uiState.value as? AccountContract.State.Error
-        assertNotNull(state)
-        assertEquals(exception.message, state.message)
-    }
+    fun `GIVEN a logged in user WHEN SignOut event SHOULD call signOutUseCase`() = runTest {
+        prepareScenario()
 
-    @Test
-    fun `GIVEN viewModel WHEN SignOut event SHOULD call signOutUseCase`() = runTest {
-        //GIVEN
-        coEvery { signOutUseCase() } returns mockk(relaxed = true)
-        //WHEN
         viewModel.emitEvent { AccountContract.Event.OnSignOutClick }
-        //THEN
+
         coVerify { signOutUseCase() }
+    }
+
+    @Test
+    fun `GIVEN logged in user WHEN SignOut event SHOULD send NavigateToAuth effect`() = runTest {
+        val effectList = mutableListOf<AccountContract.Effect>()
+        prepareScenario(effectList = effectList)
+
+        viewModel.emitEvent { AccountContract.Event.OnSignOutClick }
+
+        assertEquals(AccountContract.Effect.NavigateToAuth, effectList.first())
+    }
+
+    private fun TestScope.prepareScenario(
+        stateList: MutableList<AccountContract.State> = mutableListOf(),
+        effectList: MutableList<AccountContract.Effect> = mutableListOf(),
+        userResult: Result<User> = Result.success(User()),
+        userUiModelResult: UserUiModel = UserUiModel(),
+    ) {
+        coEvery { getCurrentUserUseCase() } returns userResult
+        every { userToUserUiModelMapper.mapFrom(any()) } returns userUiModelResult
+        coEvery { signOutUseCase() } returns Result.success(Unit)
+
+        backgroundScope.launch(UnconfinedTestDispatcher(testScheduler)) {
+            viewModel.uiState.toList(stateList)
+        }
+        backgroundScope.launch(UnconfinedTestDispatcher(testScheduler)) {
+            viewModel.uiEffect.toList(effectList)
+        }
     }
 }
---
+++ feature/groups/presentation/src/test/java/br/com/jwar/sharedbill/groups/presentation/ui/group_details/GroupDetailsViewModelTest.kt
@@ -0,0 +1,92 @@
+package br.com.jwar.sharedbill.groups.presentation.ui.group_details
+
+import br.com.jwar.sharedbill.groups.domain.model.Group
+import br.com.jwar.sharedbill.groups.domain.usecases.GetGroupByIdStreamUseCase
+import br.com.jwar.sharedbill.groups.presentation.mappers.GroupToGroupUiModelMapper
+import br.com.jwar.sharedbill.groups.presentation.models.GroupUiModel
+import br.com.jwar.sharedbill.testing.CoroutinesTestRule
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.Rule
+import org.junit.Test
+import java.util.UUID
+
+@OptIn(ExperimentalCoroutinesApi::class)
+internal class GroupDetailsViewModelTest {
+
+    @get:Rule
+    val coroutineTestRule = CoroutinesTestRule()
+
+    private val getGroupByIdStreamUseCase: GetGroupByIdStreamUseCase = mockk()
+    private val groupToGroupUiModelMapper: GroupToGroupUiModelMapper = mockk()
+    private val viewModel: GroupDetailsViewModel by lazy {
+        GroupDetailsViewModel(
+            getGroupByIdStreamUseCase = getGroupByIdStreamUseCase,
+            groupToGroupUiModelMapper = groupToGroupUiModelMapper,
+        )
+    }
+
+    @Test
+    fun onInit_shouldCallGetGroupByIdStreamUseCase() = runTest {
+        val group = Group()
+        val groupId = UUID.randomUUID().toString()
+        prepareScenario(groupResult = Result.success(group),)
+
+        viewModel.emitEvent { GroupDetailsContract.Event.OnInit(groupId) }
+
+        coVerify { getGroupByIdStreamUseCase(groupId) }
+        verify { groupToGroupUiModelMapper.mapFrom(group) }
+    }
+
+    @Test
+    fun onInit_withSucceededLoadedGroup_shouldUpdateUiState() = runTest {
+        val groupId = UUID.randomUUID().toString()
+        val groupUiModel = GroupUiModel()
+        val stateList = mutableListOf<GroupDetailsContract.State>()
+        prepareScenario(stateList = stateList, groupUiModelResult = groupUiModel)
+
+        viewModel.emitEvent { GroupDetailsContract.Event.OnInit(groupId) }
+
+        assertEquals(GroupDetailsContract.State.Loading, stateList.first())
+        assertEquals(GroupDetailsContract.State.Loaded(groupUiModel), stateList.drop(1).first())
+    }
+
+    @Test
+    fun onInit_withFailedLoadedGroup_shouldUpdateUiState() = runTest {
+        val groupId = UUID.randomUUID().toString()
+        val exception = Exception()
+        val stateList = mutableListOf<GroupDetailsContract.State>()
+        prepareScenario(stateList = stateList, groupResult = Result.failure(exception))
+
+        viewModel.emitEvent { GroupDetailsContract.Event.OnInit(groupId) }
+
+        assertEquals(GroupDetailsContract.State.Loading, stateList.first())
+        assertEquals(GroupDetailsContract.State.Error(exception.message.orEmpty()), stateList.drop(1).first())
+    }
+
+    private fun TestScope.prepareScenario(
+        stateList: MutableList<GroupDetailsContract.State> = mutableListOf(),
+        groupResult: Result<Group> = Result.success(Group()),
+        groupUiModelResult: GroupUiModel = GroupUiModel(),
+    ) {
+        coEvery { getGroupByIdStreamUseCase(any()) } returns flow {
+            emit(groupResult)
+        }
+        every { groupToGroupUiModelMapper.mapFrom(any()) } returns groupUiModelResult
+
+        backgroundScope.launch(UnconfinedTestDispatcher(testScheduler)) {
+            viewModel.uiState.toList(stateList)
+        }
+    }
+}
