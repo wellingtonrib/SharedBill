ef7f7d4e150602f2e6cca330d503273298f9faa3
Wellington <wellington.rib@gmail.com>
2024-02-03 14:37:02 -0300

    Fix splash screen behavior

--- app/src/main/java/br/com/jwar/sharedbill/MainViewModel.kt
+++ app/src/main/java/br/com/jwar/sharedbill/MainViewModel.kt
@@ -1,37 +1,32 @@
 package br.com.jwar.sharedbill
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import br.com.jwar.sharedbill.account.domain.model.User
+import br.com.jwar.sharedbill.MainContract.Effect
+import br.com.jwar.sharedbill.MainContract.Event
+import br.com.jwar.sharedbill.MainContract.State
 import br.com.jwar.sharedbill.account.domain.usecases.GetCurrentUserUseCase
-import br.com.jwar.sharedbill.account.presentation.navigation.AUTH_ROUTE
-import br.com.jwar.sharedbill.groups.presentation.ui.group_list.GROUP_LIST_ROUTE
+import br.com.jwar.sharedbill.core.common.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val getCurrentUserUseCase: GetCurrentUserUseCase
-): ViewModel() {
-
-    private val _uiState = MutableStateFlow<UiState>(UiState.Loading)
-    val uiState: StateFlow<UiState> = _uiState
-
-    init {
-        viewModelScope.launch {
-            val currentUser = getCurrentUserUseCase().getOrNull()
-            _uiState.value = UiState.Loaded(currentUser)
+): BaseViewModel<Event, State, Effect>() {
+    override fun getInitialState() = State.Initializing
+    override fun handleEvent(event: Event) {
+        when (event) {
+            is Event.OnInit -> onInit()
         }
     }
 
-    sealed class UiState {
-        object Loading : UiState()
-        data class Loaded(val currentUser: User?) : UiState()
-
-        val startDestination
-            get() = if (this is Loaded && this.currentUser != null) GROUP_LIST_ROUTE else AUTH_ROUTE
+    private fun onInit() = viewModelScope.launch {
+        val currentUser = getCurrentUserUseCase().getOrNull()
+        setState { State.Initialized(currentUser) }
+        if (currentUser == null) {
+            sendEffect { Effect.NavigateToAuth }
+        }
     }
+
 }
